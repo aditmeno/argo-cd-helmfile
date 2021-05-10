63a06807b249835582ba80b846ecf452cb9faf1a
Aditya Menon <aditya.menon@spin.pm>
2021-05-10 11:22:32 +0200

    Update helmfile version

--- src/argo-cd-helmfile.sh
+++ src/argo-cd-helmfile.sh
@@ -109,7 +109,7 @@ else
   else
     LOCAL_HELMFILE_BINARY="/tmp/__${SCRIPT_NAME}__/bin/helmfile"
     if [[ ! -x "${LOCAL_HELMFILE_BINARY}" ]]; then
-      wget -O "${LOCAL_HELMFILE_BINARY}" "https://github.com/roboll/helmfile/releases/download/v0.138.7/helmfile_linux_amd64"
+      wget -O "${LOCAL_HELMFILE_BINARY}" "https://github.com/roboll/helmfile/releases/download/v0.139.3/helmfile_linux_amd64"
       chmod +x "${LOCAL_HELMFILE_BINARY}"
     fi
     helmfile="${LOCAL_HELMFILE_BINARY}"
